9a7e8a076d52aa4389020dd66bb9fe4f214f3cf5
Sai Nandan. Pai <sainandan.p@ADKPISOFT.COM>
2022-05-08 17:12:10 +0530

    Initial commit

---
+++ src/main/java/com/smilebat/learntribe/learntribeanalytics/LearntribeAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.smilebat.learntribe.learntribeanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearntribeAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearntribeAnalyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smilebat/learntribe/learntribeanalytics/LearntribeAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smilebat.learntribe.learntribeanalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearntribeAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
